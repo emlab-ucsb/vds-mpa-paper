e87849e2a1e97aed69743c2a87c8bb9af5ec5f00
jcvdav <juancarlos.villader@gmail.com>
2018-10-03 19:51:49 -0700

    Finally got the redistribution data ready. Will run spatial analyses tomorrow

---
+++ scripts/displacement_data_ll.R
@@ -0,0 +1,74 @@
+library(startR)
+library(magrittr)
+library(sf)
+library(tidyverse)
+
+# Custom functions that should be part of startR
+source(here::here("scripts", "st_rotate.R"))
+source(here::here("scripts", "sfc_as_cols.R"))
+
+vessel_tracks <- readRDS(file = here::here("raw_data", "vessel_tracks.rds"))
+
+eez <- read_sf(dsn = here::here("raw_data", "spatial", "EEZ"), layer = "eez_v10") %>% 
+  filter(ISO_Ter1 %in% unique(vessel_tracks$eez_iso3)) %>% 
+  mutate(PNA = ISO_Ter1 %in% c("FSM", "KIR", "MHL", "NRU", "PLW", "PNG", "SLB", "TUV")) %>% 
+  st_rotate()
+
+pipa <- read_sf(dsn = here::here("data", "spatial", "PIPA"),
+                layer = "PIPA") %>% 
+  mutate(inside = T) %>% 
+  select(inside) %>% 
+  st_rotate()
+
+# Longliners
+
+memory.limit(size = 8e6)
+
+ll_rest <- vessel_tracks %>% 
+  filter(!gear == "purse_seines",
+         treated,
+         fishing,
+         !eez_iso3 %in% c("KIR")) %>% 
+  st_as_sf(coords = c(7, 8), crs = "+proj=longlat +datum=WGS84 +no_defs") %>%
+  st_rotate() %>% 
+  mutate(inside = NA) %>% 
+  sfc_as_cols(names = c("lon", "lat")) %>%
+  st_set_geometry(value = NULL) %>%
+  mutate(country = ifelse(is.na(eez_iso3), "HS", eez_iso3))
+
+ll <- vessel_tracks %>% 
+  filter(!gear == "purse_seines",
+         treated,
+         fishing,
+         eez_iso3 == "KIR") %>% 
+  st_as_sf(coords = c(7, 8), crs = "+proj=longlat +datum=WGS84 +no_defs") %>%
+  st_rotate() %>% 
+  st_join(pipa) %>% 
+  sfc_as_cols(names = c("lon", "lat")) %>% 
+  st_set_geometry(value = NULL) %>%
+  mutate(country = ifelse(is.na(inside), eez_iso3, "PIPA")) %>%
+  rbind(ll_rest) %>% 
+  group_by(year, month) %>% 
+  mutate(total_hours = sum(hours)) %>% 
+  ungroup() %>% 
+  group_by(year, month, country, total_hours) %>%
+  summarize(h = sum(hours)) %>% 
+  mutate(h_prop = h / total_hours) %>% 
+  ungroup()
+
+displacement_data_ll <- expand.grid(country = c(unique(ll$country), "HS"),
+                                    year = 2012:2017,
+                                    month = 1:12) %>% 
+  arrange(country, year, month) %>%
+  as.tibble() %>%
+  left_join(ll, by = c("country", "year", "month")) %>% 
+  mutate(h_prop = ifelse(is.na(h_prop), 0, h_prop)) %>% 
+  mutate(date = lubridate::date(paste(year, month, 1, sep = "/")),
+         country = fct_relevel(country, c("PIPA", "KIR", "HS")),
+         country = fct_relevel(country, "others", after = Inf),
+         post = year >= 2015) %>% 
+  mutate(gear = "drifting_longlines")
+
+saveRDS(object = displacement_data_ll,
+        file = here::here("data", "displacement_data_ll.rds"))
+
