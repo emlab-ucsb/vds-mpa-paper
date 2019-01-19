56a9c28f80155997659cc3ad891933ac09a0999f
jcvdav <juancarlos.villader@gmail.com>
2019-01-18 18:26:32 -0800

    Added script to calculate change in Vessel days

---
+++ scripts/change_in_vds.R
@@ -0,0 +1,35 @@
+###################################################
+#   Calculate VDs per year and per country-year   #
+###################################################
+
+# Load packages
+library(tidyverse)
+
+# Load tracks. We want to keep only the tracks of information
+# o
+# 
+# THIS IS WRONG< SHOULD BE USING VESSEL_TRACKS_BACI
+tracks <- readRDS(file = here::here("raw_data", "vessel_tracks.rds")) %>% 
+  filter(VDS,
+         gear == "purse_seines")
+  
+
+p1 <- tracks %>% 
+  filter(eez_iso3 %in% PNA_countries) %>% 
+  group_by(year) %>% 
+  summarize(total_hours = sum(hours)) %>% 
+  ggplot(aes(x = year, y = total_hours)) +
+  geom_col()
+
+ggsave(p1, filename = here::here("img", "VDS_year.pdf"))
+
+
+p2 <- tracks %>%
+  filter(eez_iso3 %in% PNA_countries) %>% 
+  group_by(year, eez_iso3) %>% 
+  summarize(total_hours = sum(hours)) %>% 
+  ggplot(aes(x = eez_iso3, y = total_hours)) +
+  geom_col() +
+  facet_wrap(~year)
+
+ggsave(p2, filename = here::here("img", "VDS_country_year.pdf"), width = 8, height = 4)
