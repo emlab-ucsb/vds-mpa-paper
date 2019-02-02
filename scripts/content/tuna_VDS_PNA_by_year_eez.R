bb3a763470a218f67a9474a21b0e065f6cc869a3
jcvdav <juancarlos.villader@gmail.com>
2019-02-01 17:35:38 -0800

    Added script that creates new VDS figures

---
+++ scripts/content/tuna_VDS_PNA_by_year_eez.R
@@ -0,0 +1,96 @@
+#################################
+#   tuna_VDS_PNA_by_year_eez    #
+#################################
+
+
+##########################################################################
+# This script calculates the number of total hours that ALL purse seiners
+# spent in PNA waters (PNA includes TKL).
+##########################################################################
+
+#### SETUP ########################################################
+# Load packages
+library(tidyverse)
+
+# Load data
+# Filter to keep only purse seiners before 2018 and modify the fishing label
+vessel_activity <- readRDS(file = here::here("raw_data",
+                                             "vessel_tracks_ALL_vessels_fished_PNA_2012_present.rds")) %>% 
+  filter(eez_iso3 %in% c("FSM",
+                         "KIR",
+                         "MHL",
+                         "NRU",
+                         "PLW",
+                         "PNG",
+                         "SLB",
+                         "TUV",
+                         "TKL"),
+         inferred_label == "tuna_purse_seines",
+         year < 2018) %>% 
+  mutate(fishing = ifelse(is.na(fishing), F, fishing))
+
+#### CREATE THE DATA ################################################
+
+# Data for yearly vessel activity (fishing not fishing)
+vessel_activity_year <- vessel_activity %>% 
+  group_by(year, fishing) %>% 
+  summarize(days = sum(days, na.rm = T)) %>% 
+  ungroup()
+
+# Data for eez-level days
+vessel_activity_year_country <- vessel_activity %>% 
+  group_by(year, eez_iso3) %>% 
+  summarize(days = sum(days, na.rm = T)) %>% 
+  ungroup()
+
+# Data for monthly activity by EEZ
+vessel_activity_month_country <- vessel_activity %>% 
+  group_by(year, month, eez_iso3) %>% 
+  summarize(days = sum(days, na.rm = T)) %>% 
+  ungroup() %>% 
+  mutate(date = lubridate::date(paste(year, month, 1, sep = "-")))
+
+#### PLOTS ########################################################
+
+# Plot for yearly PS VDS by activity
+p1 <- ggplot(vessel_activity_year, aes(x = year, y = days, fill = fishing)) +
+  geom_col(color = "black") +
+  scale_fill_brewer(palette = "Set1") +
+  cowplot::theme_cowplot() +
+  theme(text = element_text(size = 10),
+        axis.text = element_text(size = 8))+
+  labs(x = "Year", y = "Vessel-days") +
+  geom_hline(yintercept = 45000, linetype = "dashed")
+
+# Plot for yearly PS VDS by country
+p2 <- ggplot(vessel_activity_year_country, aes(x = year, y = days, fill = eez_iso3)) + 
+  geom_col(color = "black") +
+  scale_fill_brewer(palette = "Set1") +
+  cowplot::theme_cowplot() +
+  theme(text = element_text(size = 10),
+        axis.text = element_text(size = 8))+
+  labs(x = "Year", y = "Vessel-days") +
+  geom_hline(yintercept = 45000, linetype = "dashed")
+
+# Combine plots
+p12 <- cowplot::plot_grid(p1, p2, labels = "AUTO", ncol = 1)
+
+#Save plot
+ggsave(p12, filename = here::here("docs", "img", "all_TS_VDS_cty_year.pdf"), width = 6, height = 7)
+
+# Plot of monthly vessel days per EEZ
+p3 <- ggplot(vessel_activity_month_country, aes(x = date, y = days, color = eez_iso3)) + 
+  geom_line() +
+  geom_point() +
+  scale_color_brewer(palette = "Set1") +
+  cowplot::theme_cowplot() +
+  theme(text = element_text(size = 10),
+        axis.text = element_text(size = 8),
+        strip.background = element_blank())+
+  labs(x = "Year", y = "Vessel-days") +
+  facet_wrap(~eez_iso3, scales = "free_y") +
+  geom_smooth()
+
+# Save plot
+ggsave(p3, filename = here::here("docs", "img", "all_TS_VDS_cty_month.pdf"), width = 6, height = 4)
+
