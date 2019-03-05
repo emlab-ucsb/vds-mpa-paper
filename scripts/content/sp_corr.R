90eeefef8cda12a5868f3642fb7a7b005df70b2d
jcvdav <juancarlos.villader@gmail.com>
2019-03-05 12:45:01 -0800

    Updated analyses and graph to include nino34 index

--- scripts/content/sp_corr.R
+++ scripts/content/sp_corr.R
@@ -32,6 +32,10 @@ files <- list.files(path = here("data",
 # Stack files
 rs <- stack(here("data", "spatial", "monthly_rasterized_effort_by_region", files))
 
+# Nino4 index
+nino4 <- read.csv(here("data", "all_indices.csv")) %>% 
+  select(year, month = month_n, nino4anom)
+
 
 ##### CALCULATIONS ######################################################
 
@@ -52,11 +56,14 @@ corr <- raster::as.data.frame(rs, xy = T) %>%
 
 # Calculate number of cells with both vessels
 n_both <- corr %>%
-  filter(both > 0) %>% 
+  filter(both > 0) %>%
   group_by(year, month, date, post, sate1, sate2) %>% 
   summarize(n = sum(both)) %>% 
   ungroup() %>% 
-  mutate(dif = interval(date("2015-01-01"), date)%/% months(1))
+  complete(year, nesting(month), fill = list(n = 0, post = F, sate1 = 0, sate2 = 0)) %>%
+  mutate(date = date(paste(year, month, 1, sep = "-")),
+         dif = interval(date("2015-01-01"), date)%/% months(1)) %>% 
+  left_join(nino4, by = c("year", "month"))
 
 
 # Calculate spatial correlation
@@ -65,25 +72,47 @@ corr_both <- corr %>%
   group_by(year, month, date, post, sate1, sate2) %>%
   summarize(cor = cor(Control, Treated)) %>% 
   ungroup() %>% 
-  mutate(dif = interval(date("2015-01-01"), date)%/% months(1))
+  mutate(dif = interval(date("2015-01-01"), date)%/% months(1)) %>% 
+  left_join(nino4, by = c("year", "month"))
 
 
 
 #### FIT THE MODELS ######################################################
+n_model1 <- lm(n ~ dif + I(dif^2) + I(dif^3) + I(dif^4), data = n_both)
+n_model2 <- lm(n ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + nino4anom, data = n_both)
+n_model3 <- lm(n ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + sate1 + sate2, data = n_both)
+n_model4 <- lm(n ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + sate1 + sate2 + nino4anom, data = n_both)
 
-n_model <- lm(n ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + sate1 + sate2, data = n_both)
-corr_model <- lm(cor ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + sate1 + sate2, data = corr_both)
 
 
-models <- list(n_model, corr_model)
+corr_model1 <- lm(cor ~ dif + I(dif^2) + I(dif^3) + I(dif^4), data = corr_both)
+corr_model2 <- lm(cor ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + nino4anom, data = corr_both)
+corr_model3 <- lm(cor ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + sate1 + sate2, data = corr_both)
+corr_model4 <- lm(cor ~ dif + I(dif^2) + I(dif^3) + I(dif^4) + sate1 + sate2 + nino4anom, data = corr_both)
+
+
+models <- list(n_model1,
+               n_model2,
+               n_model3,
+               n_model4,
+               corr_model1,
+               corr_model2,
+               corr_model3,
+               corr_model4)
 
 stargazer::stargazer(models,
                      se = commarobust::makerobustseslist(models),
                      t.auto = T,
                      p.auto = T,
-                     single.row = T,
                      intercept.bottom = F,
-                     covariate.labels = c("Constant", "$M$", "$M^2$", "$M^3$", "$M^4$", "$\\sigma_1$", "$\\sigma_2$"),
+                     covariate.labels = c("Constant",
+                                          "M",
+                                          "M $^2$",
+                                          "M $^3$",
+                                          "M $^4$",
+                                          "NINO4",
+                                          "$\\sigma_1$",
+                                          "$\\sigma_2$"),
                      dep.var.caption = "",
                      dep.var.labels.include = F,
                      column.sep.width = "1pt",
@@ -91,55 +120,85 @@ stargazer::stargazer(models,
                      type = "latex",
                      omit = c("flag", "month"),
                      add.lines = list(
-                       c("Month FE", rep("Yes", 10)),
-                       c("Flag FE", rep("Yes", 10))),
+                       c("NINO4", rep(c("No", "Yes", "No", "Yes"), 2)),
+                       c("Satellites", c("No", "No", "Yes", "Yes"))),
                      omit.stat = c("adj.rsq", "f", "ser"),
                      header = F,
-                     # float.env = "sidewaystable",
                      title = "\\label{tab:sp_corr}Coefficient estimates for a third-polinomial fit to the measures of crowding. The first column shows coefficients for the number of cells with treated and control vessels during the same month. The second column shows coefficients for the spatial correlation for presence / absence of treated and control vessels. The explanatory variable is the number of months before implementation of PIPA. Numbers in parentheses are heteroskedastic-robust standard errors.",
                      out = here("docs", "tab", "sp_corr.tex"))
 
 
 #### PLOT THEM #########################################################
 
+# Create DF with predictions
+# n_both_pred <- n_both %>% 
+#   mutate(predicted1 = predict(n_model1),
+#          predicted2 = predict(n_model2),
+#          predicted3 = predict(n_model3),
+#          predicted4 = predict(n_model4)) %>% 
+#   select(date, predicted1, predicted2, predicted3, predicted4) %>% 
+#   gather(model, prediction, -date) %>% 
+#   mutate(groups = group_indices(., model)) %>% 
+#   left_join(tibble(groups = c(1:4),
+#                    model_2 = c("Month",
+#                                "Month + nino4",
+#                                "Month + satellites",
+#                                "Month + satellites + nino4")),
+#             by = "groups")
+# 
+# corr_both_pred <- corr_both %>% 
+#   mutate(predicted1 = predict(corr_model1),
+#          predicted2 = predict(corr_model2),
+#          predicted3 = predict(corr_model3),
+#          predicted4 = predict(corr_model4)) %>% 
+#   select(date, predicted1, predicted2, predicted3, predicted4) %>% 
+#   gather(model, prediction, -date) %>% 
+#   mutate(groups = group_indices(., model)) %>% 
+#   left_join(tibble(groups = c(1:4), model_2 = c("Month", "Month + satellites", "Month + satellites + nino4", "Month + nino4")), by = "groups")
+
 # Plot for number of cells with vessels from BOTH groups
-n_both_plot <- n_both %>% 
-  mutate(predicted = predict(n_model)) %>% 
-  ggplot(aes(x = date, y = n)) +
+n_both_plot <- ggplot(data = n_both) +
   geom_vline(xintercept = date("2015-01-01"),
              linetype = "dashed", color = "black", size = 1) +
-  geom_point(size = 4,
+  geom_point(aes(x = date, y = n, fill = nino4anom),
+             size = 4,
              alpha = 0.5,
-             fill = "#377EB8",
              color = "black",
              shape = 21) +
-  geom_line(aes(y = predicted)) +
+  geom_line(aes(x = date, y = predict(n_model1)),
+            size = 1) +
   cowplot::theme_cowplot() +
-  theme(legend.position = "none",
-        text = element_text(size = 10),
-        axis.text = element_text(size = 8)) +
-  labs(x = "Year", y = "Number of cells")
+  theme(text = element_text(size = 10),
+        axis.text = element_text(size = 8),
+        legend.text = element_text(size = 8),
+        legend.justification = c(0,1),
+        legend.position = c(0, 1.08)) +
+  guides(fill = guide_colorbar(title = "NINO4 Anomally",
+                               ticks.colour = "black",
+                               frame.colour = "black")) +
+  labs(x = "Year", y = "Number of cells") +
+  scale_fill_gradientn(colors = colorRamps::matlab.like(10))
 
 # Plot for correlations
-corr_both_plot <- corr_both %>% 
-  mutate(predicted = predict(corr_model)) %>% 
-  ggplot(aes(x = date, y = cor)) +
+corr_both_plot <- ggplot(data = corr_both) +
   geom_vline(xintercept = date("2015-01-01"),
              linetype = "dashed",
              color = "black",
              size = 1) +
-  geom_point(size = 4,
+  geom_point(aes(x = date, y = cor, fill = nino4anom),
+             size = 4,
              alpha = 0.5,
-             fill = "#377EB8",
              color = "black",
              shape = 21) +
-  geom_line(aes(y = predicted)) +
+  geom_line(aes(x = date, y = predict(corr_model1)),
+            size = 1) +
   cowplot::theme_cowplot() +
   theme(legend.position = "none",
         text = element_text(size = 10),
         axis.text = element_text(size = 8)) +
   labs(x = "Year", y = "Pearson's correlation") + 
-  scale_y_continuous(limits = c(0, 0.75))
+  scale_y_continuous(limits = c(0, 0.75)) +
+  scale_fill_gradientn(colors = colorRamps::matlab.like(10))
 
 # Export plot
 plot <- cowplot::plot_grid(n_both_plot, corr_both_plot,
