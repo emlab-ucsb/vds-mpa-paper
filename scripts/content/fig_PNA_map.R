3adcd4f0e09f22f59480e4d1f1652d4552301125
jcvdav <juancarlos.villader@gmail.com>
2019-03-06 21:39:26 -0800

    Updated PNA map by including labels of countries

--- scripts/content/fig_PNA_map.R
+++ scripts/content/fig_PNA_map.R
@@ -9,18 +9,43 @@
 #####################################################
 
 # Load packages
+library(ggrepel)
+library(ggsflabel)
+library(countrycode)
 library(startR)
 library(sf)
+library(here)
 library(tidyverse)
 
 # Source local functions
-source(here::here("scripts", "functions", "st_rotate.R"))
+source(here("scripts", "functions", "st_rotate.R"))
+source(here("scripts", "functions", "sfc_as_cols.R"))
 
 # List of countries
-countries <- c("PIPA", "KIR", "HS", "ASM", "COK", "FSM", "MHL", "NRU", "PNG", "SLB", "TKL", "TUV", "UMI", "FJI", "NIU", "TON", "WSM", "WLF", "VUT", "NCL", "PLW")
+countries <- c("PIPA",
+               "KIR",
+               "HS",
+               "ASM",
+               "COK",
+               "FSM",
+               "MHL",
+               "NRU",
+               "PNG",
+               "SLB",
+               "TKL",
+               "TUV",
+               "UMI",
+               "FJI",
+               "NIU",
+               "TON",
+               "WSM",
+               "WLF",
+               "VUT",
+               "NCL",
+               "PLW")
 
 # List of PNA countries
-PNA_countries <- c("FSM", "KIR", "MHL", "NRU", "PLW", "PNG", "SLB", "TUV")
+PNA_countries <- c("FSM", "KIR", "MHL", "NRU", "PLW", "PNG", "SLB", "TUV", "TKL")
 
 # List of countries that hold VDS rights (and then sell to others)
 VDS_countries <- c(PNA_countries, "TKL")
@@ -30,7 +55,7 @@ small_coast <- rnaturalearth::ne_countries(scale = "large", returnclass = "sf") 
   filter(sov_a3 %in% countries) %>% 
   st_rotate()
 
-eez <- read_sf(dsn = here::here("data", "spatial", "EEZ_subset"),
+eez <- read_sf(dsn = here("data", "spatial", "EEZ_subset"),
                layer = "EEZ_subset") %>% 
   filter(ISO_Ter1 %in% countries) %>% 
   mutate(KIR = ISO_Ter1 == "KIR",
@@ -38,21 +63,39 @@ eez <- read_sf(dsn = here::here("data", "spatial", "EEZ_subset"),
   group_by(ISO_Ter1, VDS, KIR) %>% 
   summarize()
 
-pipa <- sf::read_sf(dsn = here::here("data", "spatial", "PIPA"), layer = "PIPA") %>% 
+labels <- eez %>% 
+  filter(ISO_Ter1 %in% VDS_countries) %>% 
+  # lwgeom::st_make_valid() %>% 
+    mutate(label = countrycode(sourcevar = ISO_Ter1,
+                               origin = "iso3c",
+                               destination = "country.name"),
+           label = ifelse(ISO_Ter1 == "FSM", "Federal States\nof Micronesia", label)) %>% 
+  st_simplify(dTolerance = 0.1)
+
+
+pipa <- sf::read_sf(dsn = here("data", "spatial", "PIPA"), layer = "PIPA") %>% 
   sf::st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs") %>% 
   st_rotate()
 
 plot <- ggplot() +
-  geom_sf(data = eez, aes(color = KIR, fill = VDS), alpha = 0.5) +
+  geom_sf(data = eez, aes(color = KIR, fill = VDS), alpha = 0.5, size = 0.1) +
   geom_sf(data = pipa, fill = "red") +
-  geom_sf(data = small_coast, color = "black", fill = "#E3E3E3") +
+  geom_sf(data = small_coast, color = "black", fill = "#E3E3E3", size = 0.1) +
+  geom_sf_label_repel(data = labels, mapping = aes(label = ISO_Ter1), force = 400, seed = 2) +
   ggtheme_plot() +
   scale_color_manual(values = c("black", "red")) +
   scale_fill_manual(values = c("transparent", "steelblue")) +
   theme(legend.position = "none")
 
-ggsave(plot, filename = here::here("docs", "img", "PNA_map.png"), width = 6, height = 3.5)
-ggsave(plot, filename = here::here("docs", "img", "PNA_map.pdf"), width = 6, height = 3.5)
+ggsave(plot = plot,
+       filename = here("docs", "img", "PNA_map.png"),
+       width = 6,
+       height = 3.5)
+
+ggsave(plot = plot,
+       filename = here("docs", "img", "PNA_map.pdf"),
+       width = 6,
+       height = 3.5)
 
 
 
