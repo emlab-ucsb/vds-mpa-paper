8e017552414e6641087f860172423e9ccc8955b0
jcvdav <juancarlos.villader@gmail.com>
2019-03-19 07:47:56 -0700

    Updated script and data with vessel length and factors

--- scripts/collection/activity_by_vessel_year_eez_download.R
+++ scripts/collection/activity_by_vessel_year_eez_download.R
@@ -8,31 +8,15 @@
 # all vessels that ever fished inside PNA using the latest tables.
 ########################################################################
 
-# Load the packages that I need
-library(dplyr)
-library(dbplyr)
-library(DBI)
-library(bigrquery)
-library(magrittr)
-
-# Create a connection to Google BigQuery
-# For this to run, you require authentication and be part of ucsb-gfw
-BQc <- bigrquery::dbConnect(
-  drv = bigrquery::bigquery(),
-  project = "ucsb-gfw", # This is the project, to which you must belong
-  dataset = "mpa_displacement", # This is the dataset, where I keep all the tables
-  allowLargeResults = TRUE #We're downloading about 10 GB of data
-)
-
-# List all tables to test the connection
-DBI::dbListTables(BQc)
-
+# Load packages
+library(startR)
 
 # DOWNLOAD THE DATA
 # Create a tbl pointing to vessel tracks
-vessel_activity <- dplyr::tbl(BQc, "activity_by_vessel_year_eez") %>%
-  collect() # collect brings the information out of the database and into memory
-
+vessel_activity <- get_table(project = "ucsb-gfw",
+                             dataset = "mpa_displacement",
+                             table = "activity_by_vessel_year_eez")
+  
 # Save it to disk
 saveRDS(vessel_activity,
         file = here::here("raw_data", "activity_by_vessel_year_eez.rds"))
